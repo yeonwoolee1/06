ee48966a87d1967e1ab8504fe2f162e97e06bc90
yeonwoolee1 <144992849+yeonwoolee1@users.noreply.github.com>
2023-10-12 15:49:16 +0900

    실습2

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void func1(void)
+{
+	int x;
+	printf("func1 x is at %p\n", &x);
+}
+int main(void) {
+	int x;
+	printf("main x is at %p\n", &x);
+	func1();
+	
+	return 0;
+	
+	
+	
+}
