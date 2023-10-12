a7d836ee96b346781574503943e9b845d67c59d3
yeonwoolee1 <144992849+yeonwoolee1@users.noreply.github.com>
2023-10-12 17:10:40 +0900

    실습5

--- main.c
+++ main.c
@@ -2,21 +2,47 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-
+int factorial(int n)
+{	
+	int i;
+	int res=1;
+	for (i=1;i<=n;i++)
+		res=res*i;
 	
-int square(int a)
-{
-	return (a*a);
+	return res;
 }
 
-int main() 
-{
-	int a=2;
-	a= square(a);
-	printf("a=%i\n",a);
+int combination(int n, int r)
+{ 
+	int div1,div2;
+
+	div1=factorial(n);
+	div2=factorial(n-r)*factorial(r);
+
+	return (div1/div2);
+}
+
+int get_integer()
+{ 
+}
+
+int main(int argc, char *argv[]) {
+	int n,r;
+	int result;
+	 
+	
+	printf("input n:");
+	scanf("%d",&n);
+	
+ 
+	printf("input r:"); 
+	scanf("%d", &r);
+	
+
+	result=combination(n,r);
 	
 	
+
+	printf("result is %i\n", result); 
 	return 0;
-	
 }
