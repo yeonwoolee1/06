214c571ddef6630bf17c55d0a8acb606eaf1db4e
yeonwoolee1 <144992849+yeonwoolee1@users.noreply.github.com>
2023-10-15 13:13:05 +0900

    실습5 최종수정

--- main.c
+++ main.c
@@ -2,18 +2,37 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-void print_star()
-{
+int factorial(int n)
+{	
 	int i;
-	for (i=0;i<10;i++)
-	printf("*");
+	int res=1;
+	for (i=1;i<=n;i++)
+		res=res*i;
+
+	return res;
 }
 
-int main(void)
-{
-	print_star();
-	print_star();
-	print_star();
-	
+int combination(int n, int r)
+{ 
+	int div1,div2;
+	div1=factorial(n);
+	div2=factorial(n-r)*factorial(r);
+
+	return (div1/div2);
+}
+
+int main(int argc, char *argv[]) {
+	int n,r;
+	int result;
+
+
+	printf("input n:");
+	scanf("%d",&n);
+	printf("input r:"); 
+	scanf("%d", &r);
+
+	result=combination(n,r);
+
+	printf("result is %i\n", result); 
 	return 0;
 }
