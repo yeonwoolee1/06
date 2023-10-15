66a578ac99802ea724ff306d923e2ee4ef8a7af4
yeonwoolee1 <144992849+yeonwoolee1@users.noreply.github.com>
2023-10-15 13:09:56 +0900

    실습1

--- main.c
+++ main.c
@@ -2,34 +2,18 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int sumTwo(int a, int b)
+void print_star()
 {
-	return(a+b);
-}
-	
-int square(int n)
-{
-	return(n*n);
+	int i;
+	for (i=0;i<10;i++)
+	printf("*");
 }
 
-int get_max(int x, int y)
-{	if (x>y)
-		return x;
+int main(void)
+{
+	print_star();
+	print_star();
+	print_star();
 	
-		return y;
-	}
-
-int main(void) {
-	int a,b;
-	int n;
-	
-	a=3;
-	b=10;
-	printf("sunTwo result:%i\n",sumTwo(a,b) );
-	
-	n=4;
-	printf("square result:%i\n",square(n) );
-	
-	printf("get_max result:%i\n",get_max(a,b) );
 	return 0;
 }
